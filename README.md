2b775e44622c5fbbc448063afe5f34e78dea389d
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-09-16 19:21:46 +0900

    update

---
+++ README.md
@@ -0,0 +1,19 @@
+# v-lz4
+
+[lz4](github.com/lz4/lz4) V-lang port
+
+## Install
+
+```bash
+$ git clone https://github.com/funatsufumiya/v-lz4 ~/.vmodules/lz4
+```
+
+## Test
+
+```bash
+$ v test ~/.vmodules/lz4
+```
+
+## License
+
+BSD 2-Clause license
--- lz4_test.v
+++ lz4_test.v
@@ -5,32 +5,32 @@ import lz4
 fn test_lz4() {
 	// Test data
 	input := 'The quick brown fox jumps over the lazy dog.'
-	input_bytes := input.bytes().map(i8(it))
+	input_bytes := input.bytes()
 	input_len := input_bytes.len
 
 	// Allocate compression buffer
 	max_compressed_size := lz4.lz_4_compress_bound(input_len)
-	mut compressed := []i8{len: max_compressed_size, init: 0}
+	mut compressed := []u8{len: max_compressed_size, init: 0}
 
 	// Compress
 	compressed_size := lz4.lz_4_compress_default(
-		&input_bytes.data,
-		&compressed.data,
+		&u8(input_bytes.data),
+		&u8(compressed.data),
 		input_len,
 		max_compressed_size
 	)
 	assert compressed_size > 0, 'Compression failed'
 
 	// Allocate decompression buffer
-	mut decompressed := []i8{len: input_len, init: 0}
+	mut decompressed := []u8{len: input_len, init: 0}
 
 	// Decompress
 	decompressed_size := lz4.lz_4_decompress_safe(
-		&compressed.data,
-		&decompressed.data,
+		&u8(compressed.data),
+		&u8(decompressed.data),
 		compressed_size,
 		input_len
 	)
 	assert decompressed_size == input_len, 'Decompressed size does not match'
-	assert decompressed[..input_len].map(u8(it)).bytestr() == input, 'Decompressed data does not match original'
+	assert decompressed[..input_len].bytestr() == input, 'Decompressed data does not match original'
 }
